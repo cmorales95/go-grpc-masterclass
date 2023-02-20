00918cd97293d08ec18755392a90d8486ee9a147
Cristian Morales <crisdavidmm95.dev@gmail.com>
2023-02-19 19:44:13 -0500

    deadlines with context

---
+++ greet/client/greet_with_deadline.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/cmorales95/go-grpc-masterclass/greet/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func doGreetWithDeadline(c pb.GreetServiceClient, timeout time.Duration) {
+	log.Println("doGreetWithDeadline was invoked")
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	req := &pb.GreetRequest{FirstName: "Cristian"}
+
+	res, err := c.GreetWithDeadline(ctx, req)
+	if err != nil {
+		e, ok := status.FromError(err)
+		if ok {
+			if e.Code() == codes.DeadlineExceeded {
+				log.Println("Deadline exceeded")
+				return
+			}
+		} else {
+			log.Fatalf("a non gRPC error: %v\n", err)
+		}
+	}
+
+	log.Printf("GreetWithDeadline: %s\n", res.Result)
+}
