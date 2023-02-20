eed14e338c39def3585e7bfc914a3104e26c1a9f
Cristian Morales <crisdavidmm95.dev@gmail.com>
2023-02-19 20:04:09 -0500

    ssl security

--- greet/client/main.go
+++ greet/client/main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"log"
-	"time"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 
 	pb "github.com/cmorales95/go-grpc-masterclass/greet/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true // change that to true if needed
+	var opts []grpc.DialOption
+
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certificate: %v\n", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		creds := grpc.WithTransportCredentials(insecure.NewCredentials())
+		opts = append(opts, creds)
+	}
+
+	// opts = append(opts, grpc.WithChainUnaryInterceptor(LogInterceptor(), AddHeaderInterceptor()))
+
+	conn, err := grpc.Dial(addr, opts...)
 	if err != nil {
 		log.Fatalf("Failed to connect: %v\n", err)
 	}
@@ -21,9 +38,9 @@ func main() {
 
 	client := pb.NewGreetServiceClient(conn)
 
-	// doGreet(client)
+	doGreet(client)
 	// doGreetManyTimes(client)
 	// doLongGreet(client)
 	// doGreetEveryone(client)
-	doGreetWithDeadline(client, 1*time.Second)
+	// doGreetWithDeadline(client, 1*time.Second)
 }
